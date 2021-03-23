d257c063ba2bc4914758916951a8c0871c6f24b6
bandarukrishna <63196634+bandarukrishna@users.noreply.github.com>
2021-03-23 12:13:53 +0530

    Update test.java

--- eclipse-workspace/practicePrograms/src/main/java/amzon/test.java
+++ eclipse-workspace/practicePrograms/src/main/java/amzon/test.java
@@ -4,9 +4,6 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a= {1,3,5,7,9,2,4,6,8};
-		System.out.println("Enter target sum");
-		Scanner sc = new Scanner (System.in);  		
 	}
 
 }
